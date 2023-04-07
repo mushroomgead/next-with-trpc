b85d9d402b67cac1b8d99268ecae4f4de015b12d
Pornsawan Kantirangsima <mushroom.gead@gmail.com>
2023-04-07 09:49:57 +0700

    test3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { trpc } from "../utils/trpc";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const hello = trpc.getName.useQuery({ text: "x" });
+          const hello = trpc.getName.useQuery({ text: "x" });
   // const hello = trpc.hello.useQuery({ text: "xx" });
 
   if (!hello.data) {
